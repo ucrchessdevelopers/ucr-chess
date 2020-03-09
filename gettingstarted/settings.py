60dffb4bb852bf70c766faea0e21f2a3475d9175
Nick Santini <56360050+NickSantini32@users.noreply.github.com>
2020-03-08 19:58:41 -0700

    temp

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = "CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be us
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG_PROPAGATE_EXCEPTIONS = True
+ALLOWED_HOSTS = ['*'] #CHANGE ME FOR PRODUCTION
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -132,8 +133,9 @@ DATABASES['default'].update(prod_db)
 DEFAULT_FILE_STORAGE = 'db_file_storage.storage.DatabaseFileStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'gettingstarted/static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR,'gettingstarted/static')]
 STATIC_URL = "/static/"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
--- hello/models.py
+++ hello/models.py
@@ -21,6 +21,7 @@ class Player(models.Model):
     lastname = models.CharField(max_length = 30, help_text="Same as previous field; please make sure entry is correct. These are integral to how we match players to their history")
     rating = models.IntegerField(default=3000)
     rating_diff = models.IntegerField(default=0, blank=True)
+    games_played = models.IntegerField(default=0, blank=True)
     last_active = models.DateField(auto_now=False, auto_now_add=False, blank=True, default=datetime.now())
     wins = models.IntegerField(default=0, blank=True)
     losses = models.IntegerField(default=0, blank=True)
@@ -29,11 +30,21 @@ class Player(models.Model):
     def __str__(self):
         return '{}'.format(self.firstname) + ' {}'.format(self.lastname) + ': {}'.format(self.rating)
 
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+from io import TextIOWrapper
+
+# def validate_vega_chess_entry(entry):
+#     raise ValidationError(_('Line is too long'))
+
 class VegaChessEntry(models.Model):
     tournament_date = models.DateField(auto_now=False, auto_now_add=False)
-    entry = models.FileField(upload_to='hello.PictureWrapper/bytes/filename/mimetype')
-    #     validators=[FileTypeValidator(allowed_types=['text/plain'])]
-    # )
+    entry = models.FileField(upload_to='hello.PictureWrapper/bytes/filename/mimetype',
+        validators=[
+        FileTypeValidator(allowed_types=['text/plain']),
+            # validate_vega_chess_entry
+        ]
+    )
 
     def save(self, *args, **kwargs):
         delete_file_if_needed(self, 'entry')
@@ -129,31 +140,71 @@ def auto_delete_file_on_change(sender, instance, **kwargs):
         if os.path.isfile(old_file.path):
             os.remove(old_file.path)
 
-import csv
-from io import TextIOWrapper
 
 @receiver(models.signals.post_save, sender=VegaChessEntry)
 def parse_vega_chess_entry(sender, instance, **kwargs):
-    csv_file = TextIOWrapper(instance.entry)
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line = 0
-    for row in csv_reader:
-        print(f'{row[0]} | {row[1]} | {row[2]}.', flush=True)
-        line += abs(-1 - line)
+    file = TextIOWrapper(instance.entry)
+    games = []
+    names = []
+    flag = False
+    for line in file:
+        if line[0] == '-':
+            flag = True
+        elif flag and line.strip() == "":
+            break
+        elif flag:
+            firstNames.append(line.split()[1])
+            lastNames.append(line.split()[2])
+            games.append(line[line.find('|') + 1 : line.rfind('|')].split())
 
+    for i in range(len(firstNames)):
+        fn = firstNames[i]
+        ln = lastNames[i]
+        try:
+            SelectedPlayer = Player.objects.get(firstname=fn, lastname=ln)
+            SelectedPlayer.rating = SelectedPlayer.rating + 500
+            SelectedPlayer.save()
 
-        #sanat does this part
+        except Player.DoesNotExist:
+            SelectedPlayer = Player.objects.create(firstname=fn, lastname=ln)
 
+        ratings.append(SelectedPlayer.rating)
 
-    fn = "test4"
-    ln = "test4"
-    try:
-        SelectedPlayer = Player.objects.get(firstname=fn, lastname=ln)
-        SelectedPlayer.rating = SelectedPlayer.rating + 500
-        SelectedPlayer.save()
+    for row in games:
+        toAddP = []
+        toAddR = []
+        for entry in row:
+            if entry[0] == '+':
+                if entry[1:] == "BYE":
+                    toAddR.append(0.5)
+                    toAddP.append(int(entry[2:]) - 1)
+                    continue
+                else:
+                    toAddR.append(1)
+            elif entry[0] == '-':
+                if entry[1] != '-':
+                    toAddR.append(0)
+                else:
+                    toAddR.append(0.5)
+                    toAddP.append(int(entry[2:]) - 1)
+                    continue
+            elif entry[0] == '=':
+                toAddR.append(0.5)
 
-    except Player.DoesNotExist:
-        SelectedPlayer = Player.objects.create(firstname=fn, lastname=ln)
+            toAddP.append(int(entry[2:]) - 1)
+
+        gamePlayers.append(toAddP)
+        gameResults.append(toAddR)
+
+    # fn = "test4"
+    # ln = "test4"
+    # try:
+    #     SelectedPlayer = Player.objects.get(firstname=fn, lastname=ln)
+    #     SelectedPlayer.rating = SelectedPlayer.rating + 500
+    #     SelectedPlayer.save()
+    #
+    # except Player.DoesNotExist:
+    #     SelectedPlayer = Player.objects.create(firstname=fn, lastname=ln)
 
     instance.delete()
 
