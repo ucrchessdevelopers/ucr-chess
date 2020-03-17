d5f4a23fe9cc5af12c620decc1c1200ba48e2be3
Sanat Mishra <sanatmishra2222@gmail.com>
2020-03-17 14:49:59 -0700

    removed debug ststements

--- hello/models/VegaChessEntry.py
+++ hello/models/VegaChessEntry.py
@@ -50,10 +50,10 @@ def centerProvisionalPlayers(provisional, ratings, gamePlayers, gameResults):
             opp = gamePlayers[provIndices[i]][j]
             if provisional[opp]:
                 provMatrix[i][reversePImap[opp]] -= 1
-                provMatrix[i][provIndices.size] += 800*gameResults[provIndices[i]][j] - 400
+                provMatrix[i][provIndices.size] += 400*gameResults[provIndices[i]][j] - 200
             else:
                 ratingAnchor = True
-                provMatrix[i][provIndices.size] += ratings[opp] + 800*gameResults[provIndices[i]][j] - 400
+                provMatrix[i][provIndices.size] += ratings[opp] + 400*gameResults[provIndices[i]][j] - 200
         provMatrix[i][i] += gamePlayers.shape[1]
 
     # To prevent the matrix from being degenerate, which it SHOULD be in this case, but we're cheating around it anyways...
@@ -62,13 +62,10 @@ def centerProvisionalPlayers(provisional, ratings, gamePlayers, gameResults):
         provMatrix[0][0] += 1
 
     newMat = sp.Matrix(provMatrix).rref()
-    print(newMat)
     lastCol = np.array(newMat[0].col(-1).transpose())
-    print(lastCol)
 
     if not ratingAnchor:
         lastCol += 1500 - np.sum(lastCol)/lastCol.size
-        print(lastCol)
 
     for x, y in reversePImap.items():
         ratings[x] = lastCol[0][y]
@@ -90,8 +87,6 @@ def parse_vega_chess_entry(sender, instance, **kwargs):
             firstNames.append(line.split()[1])
             lastNames.append(line.split()[2])
             games.append(line[line.find('|') + 1 : line.rfind('|')].split())
-
-    print(games)
 
     for i in range(len(firstNames)):
         fn = firstNames[i]
@@ -126,56 +121,12 @@ def parse_vega_chess_entry(sender, instance, **kwargs):
                 gameResults[i][j] = 0.5
             gamePlayers[i][j] = int(games[i][j][2:]) - 1
     provisionalN = np.asarray([True for i in range(len(ratings))])
-    #provisionalN[2] = False
-    #provisionalN[6] = False
     ratingsN =  np.asarray(ratings, dtype="float64")
     gamePlayersN = np.asarray(gamePlayers)
     gameResultsN = np.asarray(gameResults)
-    print(ratings)
-    print(provisionalN)
-    print(ratingsN)
-    print(gameResultsN)
-    print(np.sum(gameResultsN == 0), np.sum(gameResultsN == 1))
     centerProvisionalPlayers(provisionalN, ratingsN, gamePlayersN, gameResultsN)
-    print(ratingsN)
     adjustEloFromAllGames(provisionalN, ratingsN, gameResultsN, gamePlayersN, 32)
-    print(ratingsN)
 
-    # for row in games:
-    #     toAddP = []
-    #     toAddR = []
-    #     for entry in row:
-    #         if entry[0] == '+':
-    #             if entry[1:] == "BYE":
-    #                 toAddR.append(0.5)
-    #                 toAddP.append(int(entry[2:]) - 1)
-    #                 continue
-    #             else:
-    #                 toAddR.append(1)
-    #         elif entry[0] == '-':
-    #             if entry[1] != '-':
-    #                 toAddR.append(0)
-    #             else:
-    #                 toAddR.append(0.5)
-    #                 toAddP.append(int(entry[2:]) - 1)
-    #                 continue
-    #         elif entry[0] == '=':
-    #             toAddR.append(0.5)
-    #
-    #         toAddP.append(int(entry[2:]) - 1)
-    #
-    #     gamePlayers.append(toAddP)
-    #     gameResults.append(toAddR)
-
-    # fn = "test4"
-    # ln = "test4"
-    # try:
-    #     SelectedPlayer = Player.objects.get(firstname=fn, lastname=ln)
-    #     SelectedPlayer.rating = SelectedPlayer.rating + 500
-    #     SelectedPlayer.save()
-    #
-    # except Player.DoesNotExist:
-    #     SelectedPlayer = Player.objects.create(firstname=fn, lastname=ln)
     for i in range(len(firstNames)):
         fn = firstNames[i]
         ln = lastNames[i]
@@ -184,8 +135,8 @@ def parse_vega_chess_entry(sender, instance, **kwargs):
 
 
         SelectedPlayer.last_active = instance.tournament_date
-        SelectedPlayer.rating_diff = ratings[i] - SelectedPlayer.rating
-        SelectedPlayer.rating = ratings[i]
+        SelectedPlayer.rating_diff = ratingsN[i] - SelectedPlayer.rating
+        SelectedPlayer.rating = ratingsN[i]
         SelectedPlayer.save()
 
 
